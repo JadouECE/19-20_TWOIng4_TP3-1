ae5b8848abdc1e9dffdae26500cc5b8d95e1eef1
JadouECE <jade.laville@edu.ece.fr>
2019-11-15 10:58:01 +0100

    Preesque

--- src/index.js
+++ src/index.js
@@ -10,37 +10,86 @@ import './index.css';
 
 
 
+class Profil extends React.Component{
 
-class Boutons extends React.Component{
-	
+	constructor(props){
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.state={
+			pageJ:{
+				id:1,
+				profil:{
+				prenom:"Jeanne",
+				nom:"Dupont",
+				date:"1er Juin 1966",
+				image:"jeanne.png"
+				},
+				like:0
+			},
 
-	render(){
-	
-		return (
-		<div className="3_btn">
-		<p className="fb">Facebook</p>
-			<button class btn_nom> Jeanne </button>
-			<button class btn_nom> Martine </button>
-			<button class btn_nom> Claude </button>
-		</div>
-		);
+			pageM:{
+				id:2,
+				profil:{
+				prenom:"Martine",
+				nom:"Dupont",
+				date:"3 Juin 1976",
+				image:"loic.png"
+				},
+				like:0
+			},
+			pageC:{
+				id:3,
+				profil:{
+				prenom:"Claude",
+				nom:"Dupont",
+				date:"1er Juillet 2006",
+				image:"bernard.png"
+				},
+				like:0
+			},
 
+			pageactuel:""
+
+		};
+
+		this.state.pageactuel=this.state.pageJ;
 	}
 
-}
+	handleClick = (id) => {
 
-class Profil extends React.Component{
+		if (id===1)
+		{
+		this.setState ({pageactuel : this.state.pageJ});
+		}
+		else if (id===2)
+		{
+		this.setState ({pageactuel : this.state.pageM});
+		}
+		else if (id===3)
+		{
+		this.setState ({pageactuel : this.state.pageC});
+		}
+	  
+	}
 
 	render(){
 		return (
 			<div class="profil">
-			
-			<img src="jeanne.jpg" alt="Jeanne"/>
-			<p class="Nom">Jeanne</p>
-			<p class="prenom">Dupont</p>
-			<p class="date">1er Juin 1966</p>
-			<h2>Dernière publication</h2>
-			<p>enant compte de leurs interactions. Cet ensemble, qui contient les êtres vivants, leur milieu de vie et les relations qu'ils entretiennent, forme un écosystème. </p>
+
+				<div className="3_btn">
+					<p className="fb">Facebook</p>
+					<button className="jeanne" id={this.state.pageJ.id} onClick={this.handleClick("1")}> Jeanne </button>
+					<button className="Martine" id={this.state.pageM.id} onClick={this.handleClick("2")}> Martine </button>
+					<button className="Claude" id={this.state.pageC.id} onClick={this.handleClick("3")}> Claude </button>
+				</div>
+		
+				<img src="jeanne.jpg" alt="Jeanne"/>
+				<p className="Nom">{this.state.pageactuel.profil.prenom}</p>
+				<p className="prenom">{this.state.pageactuel.profil.nom}</p>
+				<p className="date">{this.state.pageactuel.profil.date}</p>
+				<h2>Dernière publication</h2>
+				<p>enant compte de leurs interactions. Cet ensemble, qui contient les êtres vivants, leur milieu de vie et les relations qu'ils entretiennent, forme un écosystème. </p>
+
 			</div>
 
 		);
@@ -51,7 +100,6 @@ class FaceB extends React.Component {
 	render() {
 		return(
 			<div className="navig">
-			<Boutons />
 			<Profil/>
 			</div>
 		);
